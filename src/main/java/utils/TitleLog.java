49cb3ab47533dd14867bdc0aa121a3123ce43710
Samuel <samuelsoutosilva14@gmail.com>
2023-04-23 19:09:45 -0300

    feat: add title log utls

---
+++ src/main/java/utils/TitleLog.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TitleLog {
+
+  private TitleLog() {}
+
+  public static void execute(String title) {
+    System.out.println(title);
+  }
+
+  public static void linebreak() {
+    System.out.println();
+  }
+}
